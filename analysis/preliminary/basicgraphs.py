4affa9f4f96f3664bc514f1ce78bd045076ddb90
leolmeta <lolmetab@gmail.com>
2025-09-30 15:12:04 -0500

    changed to function for graph biulding and adjusted graph appearances

--- analysis/preliminary/basicgraphs.py
+++ analysis/preliminary/basicgraphs.py
@@ -3,50 +3,54 @@ import sklearn
 import matplotlib.pyplot as plt
 
 data = pd.read_csv('data/2425_outfield_finalists.csv')
+data['Per 90 Minutes_npxG+xAG'] = data['Expected_npxG+xAG'] / data['Playing Time_90s']
 
-data = data.sort_values(by='Expected_npxG+xAG')
+def bar_graph(df, x_col, y_col, xlabel, ylabel, title, graph_name):
+    df = df.sort_values(by=y_col)
 
-plt.rcParams['font.family'] = 'DejaVu Sans'
+    plt.rcParams['font.family'] = 'DejaVu Sans'
 
-plt.bar(data['player'],data['Expected_npxG+xAG'])
-plt.xlabel('Name')
-plt.ylabel('Non-Penalty xG + xA')
-plt.title("2025 Outfield Ballon d'Or Finalists Ranked by Expected Non-Penalty Goal Contributions")
-plt.xticks(rotation=90)
+    plt.bar(df[x_col], df[y_col])
+    plt.xticks(rotation=90)
 
-fig = plt.gcf()
-ax = plt.gca()
+    fig = plt.gcf()
+    ax = plt.gca()
 
-fig.patch.set_facecolor('black')
-ax.set_facecolor('black')
+    fig.patch.set_facecolor('black')
+    ax.set_facecolor('black')
 
-for bar in ax.patches:
-    bar.set_color('white')
+    for bar in ax.patches:
+        bar.set_color('white')
 
-for spine in ax.spines.values():
-    spine.set_color('yellow')
+    for spine in ax.spines.values():
+        spine.set_color('yellow')
 
-ax.tick_params(axis='x', colors='white', which='both', direction='out')
-ax.tick_params(axis='y', colors='white', which='both', direction='out')
-ax.xaxis.set_tick_params(color='yellow')
-ax.yaxis.set_tick_params(color='yellow')
+    ax.tick_params(axis='x', colors='white', which='both', direction='out')
+    ax.tick_params(axis='y', colors='white', which='both', direction='out')
+    ax.xaxis.set_tick_params(color='yellow')
+    ax.yaxis.set_tick_params(color='yellow')
 
-ax.set_xlabel("Name", color='white')
-ax.set_ylabel("Non-Penalty xG + xA", color='white')
-ax.set_title("2025 Outfield Ballon d'Or Finalists Ranked by Expected Non-Penalty Goal Contributions", color='white')
+    ax.set_xlabel(xlabel, color='white')
+    ax.set_ylabel(ylabel, color='white')
+    ax.set_title(title, color='white')
 
-for bar in ax.patches:
-    height = bar.get_height() + 0.4
-    ax.text(
-        bar.get_x() + bar.get_width() / 2, height,
-        f'{height:.1f}',
-        ha='center', va='bottom', color='yellow', fontsize=8, rotation=90
-    )
+    max_bar_height = ax.patches[-1].get_height()
 
-ax.set_ylim(0, max(data['Expected_npxG+xAG']) * 1.12)
+    for bar in ax.patches:
+        height = bar.get_height() + max_bar_height * 0.015
+        ax.text(
+            bar.get_x() + bar.get_width() / 2, height,
+            f'{height:.1f}',
+            ha='center', va='bottom', color='yellow', fontsize=8, rotation=90
+        )
 
-bars = ax.bar(data['player'], data['Expected_npxG+xAG'], color='white', width=0.9)
-ax.set_xlim(-0.5, len(data['player']) - 0.5)
+    ax.set_ylim(0, max(df[y_col]) * 1.12)
 
-plt.savefig('analysis/graphs/nonpenxgxa.png', dpi=300, bbox_inches='tight')
-plt.close()
+    bars = ax.bar(df[x_col], df[y_col], color='white', width=0.9)
+    ax.set_xlim(-0.5, len(df[x_col]) - 0.5)
+
+    plt.savefig(f'analysis/graphs/{graph_name}.png', dpi=300, bbox_inches='tight')
+    plt.close()
+
+bar_graph(data, 'player', 'Expected_npxG+xAG', 'Name','Non-Penalty Expected Goal Contributions', "2025 Outfield Ballon d'Or Finalists Ranked by Non-Penalty Expected Goal Contributions",'nonpenxgxa')
+bar_graph(data, 'player', 'Per 90 Minutes_npxG+xAG', 'Name','Non-Penalty Expected Goal Contributions Per 90', "2025 Outfield Ballon d'Or Finalists Ranked by Non-Penalty Expected Goal Contributions Per 90",'nonpenxgxaper90')
