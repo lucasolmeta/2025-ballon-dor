fixed error in correlation calculation and updated spider chart

analysis/retrospective/past_profiles.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from helper_functions.bar_graph import bar_graph
 
 data = pd.read_csv('data/nominees_outfield.csv')
 
@@ -9,7 +10,7 @@ data = data.rename(columns={
     'Take-Ons Att': 'Take-On Attempts',
     'Pass Types TB': 'Through Balls',
     'Playing Time Mn/MP': 'Minutes Per Match Played',
-    'Aerial Duels Won%': 'Aerial Duel Win Percentage', 
+    'Blocks Sh': 'Blocked Shots',
     'Performance Fld': 'Fouls Drawn',
     'Expected Np:G-xG': 'xG Overperformance',
     'Per 90 Minutes G+A-PK': 'Non-Penalty Goals and Assists Per 90'
@@ -17,12 +18,12 @@ data = data.rename(columns={
 
 stat_cols = [
     'Carries in the Final Third', 
-    'Take-On Attempts',
+    'xG Overperformance',
     'Through Balls',
     'Minutes Per Match Played',
-    'Aerial Duel Win Percentage', 
     'Fouls Drawn',
-    'xG Overperformance',
+    'Blocked Shots', 
+    'Take-On Attempts',
     'Non-Penalty Goals and Assists Per 90'
 ]
 
@@ -50,6 +51,8 @@ for col in stat_cols:
     col_max = profiles[col].max()
  
     profiles[col] = (profiles[col] - col_min) / (col_max - col_min)
+
+    profiles[col] = profiles[col].clip(lower=0.05)
 
 angles = np.linspace(0, 2 * np.pi, len(stat_cols), endpoint=False).tolist()
 angles += [angles[0]]
@@ -81,19 +84,14 @@ for label, row in profiles.groupby('Name'):
     color = colors.get(label, 'gray')
     ax.plot(angles, vals, color=color, linewidth=lw, label=label)
 
-ax.set_xticks(angles[:-1])
-ax.set_xticklabels(stat_cols, fontsize=8)
-ax.set_yticks([0.33,0.67])
-ax.set_yticklabels([])
-
 for i, label in enumerate(ax.get_xticklabels()):
-    if stat_cols[i] in ['Aerial Duel Win Percentage','Non-Penalty Goals and Assists Per 90']:
+    if stat_cols[i] == 'Non-Penalty Goals and Assists Per 90':
         label.set_y(label.get_position()[1] - 0.2)
     elif stat_cols[i] == 'Carries in the Final Third':
         label.set_y(label.get_position()[1] - 0.18)
-    elif stat_cols[i] == 'Minutes Per Match Played':
+    elif stat_cols[i] in ['Minutes Per Match Played', 'xG Overperformance']:
         label.set_y(label.get_position()[1] - 0.12)
-    elif stat_cols[i] in ['Fouls Drawn','Take-On Attempts']:
+    elif stat_cols[i] in ['Fouls Drawn','Blocked Shots',]:
         label.set_y(label.get_position()[1] - 0.07)
 
 ax.legend(loc='upper left', bbox_to_anchor=(1.1, 1.1), frameon=True)